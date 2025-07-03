d258289259ded876f45a40d9532671f2cfaf11f2
swasteee <swasteekhadka@gmail.com>
2025-07-03 22:27:47 +0545

    api

--- Flutter/faircircle/lib/data/repositories/product_repository_impl.dart
+++ Flutter/faircircle/lib/data/repositories/product_repository_impl.dart
@@ -2,26 +2,29 @@ import 'package:faircircle/data/datasource/product_datasource.dart';
 import 'package:faircircle/data/model/product_model.dart';
 import 'package:faircircle/domain/entities/product_entity.dart';
 
-
 class ProductRepositoryImpl extends ProductRepository {
-  final ProductLocalDataSource localDataSource;
+  final ProductLocalDataSource? localDataSource;
+  final ProductRemoteDataSource remoteDataSource;
 
-  ProductRepositoryImpl(this.localDataSource);
+  ProductRepositoryImpl({this.localDataSource, required this.remoteDataSource});
 
   List<ProductEntity> getAllProducts() {
-    return localDataSource.getAll();
+    if (localDataSource == null) return [];
+    return localDataSource!.getAll();
   }
 
   Future<void> addProduct(ProductEntity product) async {
+    if (localDataSource == null) return;
     final model = ProductModel(
       title: product.title,
       imagePath: product.imagePath,
     );
-    await localDataSource.add(model);
+    await localDataSource!.add(model);
+  }
+
+  Future<List<ProductEntity>> fetchProductsFromApi() async {
+    return await remoteDataSource.fetchProducts();
   }
 }
 
-class ProductRepository {
-}
-
-
+class ProductRepository {}
